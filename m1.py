3f16fda451fd5887c0b820d654b7ca96d2c5685a
Asrorbek0516 <asrorbekalimqulov@mail.com>
2026-07-14 12:45:37 +0500

    1-misol

---
+++ m1.py
@@ -0,0 +1,13 @@
+# file_name = "salom.txt"
+
+# text = "salom dunyo"
+
+# with open(file_name,'w') as f:
+#     data = f.write(text)
+
+# print(data)
+
+with open("salom.txt", 'r') as f:
+    data = f.read()
+print(data)
+
